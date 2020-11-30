f8d04017ddfa9ffa5743a0ab758cd6519d446bfb
felipecarvalhogodoi98 <felipecarvalhogodoi98@gmail.com>
2020-11-30 18:23:44 -0300

    REO4 'fadd'

--- fortnight03/cpu/basiccpu/BasicCPU.cpp
+++ fortnight03/cpu/basiccpu/BasicCPU.cpp
@@ -32,9 +32,10 @@
 
    ----------------------------------------------------------------------------
 */
-
+#include <iostream>
 #include "BasicCPU.h"
 #include "Util.h"
+using namespace std;
 
 BasicCPU::BasicCPU(Memory *memory) {
 	this->memory = memory;
@@ -127,8 +128,10 @@ int BasicCPU::ID()
 		// implementar o GRUPO A SEGUIR
 		//
 		// x111 Data Processing -- Scalar Floating-Point and Advanced SIMD on page C4-288
-
-		
+		case 0x0E000000:
+		case 0x1E000000:
+			return decodeDataProcFloat();
+			break;
 		// ATIVIDADE FUTURA
 		// implementar os DOIS GRUPOS A SEGUIR
 		//
@@ -347,6 +350,39 @@ int BasicCPU::decodeDataProcFloat() {
 			
 			return 0;
 
+			
+		case 0x1E202800:
+			//C7.2.43 FADD (scalar) on page C7-1346
+			
+			// implementado apenas ftype='00'
+			if (IR & 0x00C00000) return 1;
+
+			fpOp = FPOpFlag::FP_REG_32;
+			
+			// ler A e B
+			n = (IR & 0x000003E0) >> 5;
+			A = getSasInt(n); // 32-bit variant
+
+			m = (IR & 0x001F0000) >> 16;
+			B = getSasInt(m);
+
+			// registrador destino
+			d = (IR & 0x0000001F);
+			Rd = &(V[d]);
+
+			// atribuir ALUctrl
+			ALUctrl = ALUctrlFlag::ADD;
+			
+			// atribuir MEMctrl
+			MEMctrl = MEMctrlFlag::MEM_NONE;
+			
+			// atribuir WBctrl
+			WBctrl = WBctrlFlag::RegWrite;
+			
+			// atribuir MemtoReg
+			MemtoReg = false;
+
+			return 0;
 		default:
 			// instrução não implementada
 			return 1;
@@ -381,6 +417,9 @@ int BasicCPU::EXI()
 	{
 		case ALUctrlFlag::SUB:
 			ALUout = A - B;
+			return 0;
+		case ALUctrlFlag::ADD:
+			ALUout = A + B;
 			return 0;
 		default:
 			// Controle não implementado
@@ -421,6 +460,9 @@ int BasicCPU::EXF()
 		{
 			case ALUctrlFlag::SUB:
 				ALUout = Util::floatAsUint64Low(fA - fB);
+				return 0;
+			case ALUctrlFlag::ADD:
+				ALUout = Util::floatAsUint64Low(fA + fB);
 				return 0;
 			default:
 				// Controle não implementado
